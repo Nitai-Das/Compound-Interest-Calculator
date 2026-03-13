e4efa0e77fcafcd89e3cc16662b33e1504d5ae80
Nits <nitai.somakantan@gmail.com>
2026-03-14 01:41:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+const clear = document.getElementById('clear');
+
+ btn.addEventListener('click',function(event){
+    event.preventDefault();
+
+    const principle = document.getElementById('principle').value;
+    const rate = document.getElementById('rate').value;
+    const years = document.getElementById('years').value;
+
+    if(principle == "" || rate == "" || years == ""){
+        result.innerText = "Fill up all the fields!"
+        return;
+    }else{
+      const compint = (principle * (1 + (rate/100))**years);
+      
+      result.innerText = `your interest is $${compint.toFixed(2)}`;
+    }
+ 
+ });
+
+ clear.addEventListener('click', function(){
+    result.innerText = "";
+ });
